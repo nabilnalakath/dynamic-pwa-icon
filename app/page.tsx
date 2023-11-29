d0050091cf34b8602dcf3ebe19c8f28244312004
Nabil Nalakath <nabil@betaflux.co>
2023-11-29 15:41:13 +0530

    updated api

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,41 @@ import reactManifest from "react-manifest";
 
 export default function Home() {
  
-  const [name,setName] = useState(null);
+  const [name,setName] = useState('test app');
   let subdomain ='localhost';
   if (typeof window !== "undefined") {
     subdomain = window?.location.hostname.split(".")[0];
   }
+
+  const updateManifest = () => {
+    const manifestDetails = {
+
+    "name": "My Web App",
+    "short_name": "My App",
+    "start_url": "index.html",
+    "display": "standalone",
+    "orientation": "portrait",
+    "theme_color": "#000000",
+    "background_color": "#ffffff",
+    "icons": [
+      {
+        "src": "https://images.placeholders.dev/?width=200&height=200&text=cc%201%20app&textColor=red",
+        "sizes": "192x192",
+        "type": "image/png"
+        },
+    ],
+
+}
+
+reactManifest.update(manifestDetails, "#manifest-placeholder")
+  }
+
+
+
   useEffect(() => {
     // Fetch custom manifest when component mounts
-    fetchCustomManifest();
+   // fetchCustomManifest();
+   updateManifest
   }, []);
 
   const fetchCustomManifest = async () => {
