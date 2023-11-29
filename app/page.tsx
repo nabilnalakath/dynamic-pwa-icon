389b9f853f63cc71fc5ee1f70894ba707cdc6d1b
Nabil Nalakath <nabil@betaflux.co>
2023-11-29 15:35:23 +0530

    updated api

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 import Head from "next/head";
 import { getAppData } from "../utils/helper";
 import React, { useEffect, useState } from "react";
+import reactManifest from "react-manifest";
+
 
 export default function Home() {
-  const [dynamicManifest, setDynamicManifest] = useState(null);
-  const [name,setName] = useState("site name");
+ 
+  const [name,setName] = useState(null);
   let subdomain ='localhost';
   if (typeof window !== "undefined") {
     subdomain = window?.location.hostname.split(".")[0];
@@ -18,14 +20,14 @@ export default function Home() {
   const fetchCustomManifest = async () => {
     try {
      
-     
-      const response = await fetch('https://mocki.io/v1/8fbe1c94-9397-4b77-8fac-85a446baff6b');
+      let dynamicManifest;
+      const response = await fetch('https://mocki.io/v1/8681f059-86ee-404b-ab84-56e8097fea9c');
       let manifestData = await response.json();
       
       console.log('manifestData',manifestData[subdomain]);
-      setDynamicManifest(manifestData[subdomain]);
-     
-      
+      dynamicManifest = manifestData[subdomain];
+      setName(dynamicManifest.name)
+      reactManifest.update(dynamicManifest, "#manifest-placeholder");
     } catch (error) {
       console.error("Error fetching custom manifest:", error);
     }
@@ -35,11 +37,11 @@ export default function Home() {
   return (
     <div>
       <Head>
-      {dynamicManifest &&
+     { name && 
       <> 
         <title>{name}</title>
       
-        <link rel="manifest" href={dynamicManifest} />
+       
         <meta
           name="description"
           content={`${name} - Customized for ${subdomain}`}
@@ -48,8 +50,9 @@ export default function Home() {
         <meta name="apple-mobile-web-app-title" content={name} />
         <meta name="application-name" content={name} />
         </>
-      }
+}
       </Head>
+      
       <main>
         <h1>Hello, {name}!</h1>
         {/* Rest of your app */}
